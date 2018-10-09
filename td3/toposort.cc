812ff54549450ca1fec88753c72751ac855576f4
root <root@localhost.localdomain>
2018-10-09 14:03:58 +0200

    toposort

--- td3/toposort.cc
+++ td3/toposort.cc
@@ -9,29 +9,29 @@ vector<int> TopologicalSort(const DirectedGraph& graph){
     vector<int> triTopologique; 
     vector<vector<int>> parent(graph.NumNodes()); 
     vector<int> degre(graph.NumNodes());
-    queue<int> fifo; 
+    vector<int> fifo; 
 
     for(int i =0 ; i < graph.NumNodes() ; i++ ){
 
         if (graph.Neighbors(i).empty()){
-            fifo.push(i);
+            fifo.push_back(i);
         }else{
             for(int j = 0 ; j < graph.Neighbors(i).size() ; j++ ){
                 parent[graph.Neighbors(i)[j]].push_back(graph.Neighbors(i)[j]);
             }
         }
-        degre[i]= graph.OutDegree(i);
-    }
-
-    for (int i=0 ; i<fifo.size() ; i++){
-        fifo.pop();
-        triTopologique.push_back(fifo.front());
-            for(int j=0 ; j<parent[i].size(); j++){
-                degre[parent[i][j]]= degre[parent[i][j]]-1;
+       int k=0;
+       while(!fifo.empty()) {
+           triTopologique.push_back(fifo[k]);
+            for(int j=0 ; j <parent[i].size(); j++){
+                degre[parent[i][j]]-=1;
                 if(degre[parent[i][j]]==0){
-                    fifo.push(parent[i][j]);
+                    fifo.push_back(parent[i][j]);
                 }
             }
-    }
+       }
+    
+
+    
    return triTopologique;
 }
